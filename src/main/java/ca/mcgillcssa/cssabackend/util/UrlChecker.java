8ff7e4c63f7efab0371d21af0bec1a8eae99926c
KILLERWHOPPER <zhang1029946339@gmail.com>
2023-05-21 22:28:23 -0400

    [doc] javadoc for UrlChecker tool class

--- src/main/java/ca/mcgillcssa/cssabackend/util/UrlChecker.java
+++ src/main/java/ca/mcgillcssa/cssabackend/util/UrlChecker.java
@@ -14,6 +14,15 @@ public class UrlChecker {
         this.url = imgUrl;
     }
 
+    /**
+     * Add https:// if not present to the url, and check if the url is valid
+     * 
+     * @param imgUrl the url
+     * @return UrlChecker object containing the result of the check and the modified
+     *         url
+     * @throws IOException if the url is invalid
+     * @author Zihan Zhang
+     */
     public static UrlChecker isValidUrl(String imgUrl) throws IOException {
         if (!imgUrl.startsWith("http://") && !imgUrl.startsWith("https://")) {
             imgUrl = "https://" + imgUrl;
